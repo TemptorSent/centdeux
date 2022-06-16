6a2ecfd937b5f5df92ed97b2af1e7858ec4c822f
Pavel Fedin <pavel_fedin@mail.ru>
2022-06-17 00:19:37 +0300

    centfs/centfs-ls: Dump more data
    Use a compact format and print all known details for each file. Add comments
    with a description of what we currently know about the filesystem

--- Storage/centfs/tools/centfs-ls.c
+++ Storage/centfs/tools/centfs-ls.c
@@ -3,12 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+ * In fact it turns out that what we have here is not a real
+ * Centurion filesystem, but something like a tar archive. The real
+ * filesystem, as understood from WIPL, has a different structure.
+ * One directory entry occupies exactly 16 bytes; the first entry is
+ * skipped when searching a file, presumably it is a volume header. Nothing
+ * is known about the volume header's actual structure except that word at
+ * offset 14 points to the first sector, where filesystem metadata (let's call
+ * it disk map) is stored. The rest of entries have the following format:
+ * struct DirEntry {
+ *     char     name[10];   // File name, exactly 10 chars, padded up with spaces
+ *     uint8_t  map_entry;  // Index of the first entry in the disk map sector, denoted below
+ *     uint16_t map_sector; // Disk map sector, where the file begins (relative to disk map start)
+ *     uint8_t  file_type;  // The bootloader checks for file_type & 0x0f == 4
+ *     uint16_t unknown;
+ * };
+ * The directory terminates with first two characters of name containing bytes 0x84 0x0d
+ * The disk map consists of 3-byte entries:
+ * struct DiskMapEntry {
+ *     uint8_t  entry_type;
+ *     uint16_t payload;
+ * };
+ * "payload" has different meaning depending on entry_type:
+ * entry_type > 0 - payload specifies starting sector (absolute) of the data cluster
+ * entry_type < 0 - ~entry_type is a number of the next disk map sector (relative to disk map start),
+ *                  and payload's LSB specifies an index of the starting DiskMapEntry in that sector
+ * The first two map entries of a file constitute a header. We know nothing of the first entry,
+ * payload of the second entry yields file cluster size by doing 1 << entry.payload
+ * 
+ * The structure below appears to be a combo of a volume header and DirEntry. We may presume
+ * that the first 16 bytes are the same as on a bootable volume; it's logical to assume that a
+ * volume has a name. Then we have a 0x848d terminator, which effectively makes the volume
+ * containing zero files from WIPL's point of view. The data, following the terminator, effectively
+ * repeats the original DirEntry the file was backed up from, at least in the image we have
+ * dumping them reveals very consistent values. file_type also matches what's expected; @LOAD
+ * file that we have in the archive has a type value of 0x14, which matches WIPL's expectations.
+ * We may also speculate that the unknown field is also preserved, but we can't tell anything
+ * about its actual meaning.
+ * 
+ * So, it appears that each file in the archive is contained inside its own "mini-volume",
+ * and the archive is a chain of these "mini-volumes". The data appear contiguous, the disk
+ * map is likely not present because map_start_sector in our image contains a value of 0x5779,
+ * which does not represent a valid sector number (unless the archive spans accross several
+ * Hawk removable platters, each of which constitutes a separate physical unit)
+ */
 typedef struct cent_dirent_t {
 	char volname[10];
-	uint8_t b0a_0f[6];
-	uint8_t filemark[2];
-	char filename[10];
-	uint8_t b1c_1e[3];
+	uint8_t b0a_0d[4];
+	uint8_t map_start_sector[2];
+	uint8_t filemark[2];         /* Contains 0x848d */
+	char filename[10];           /* VolumeEntry, shifted by 2 bytes */
+	uint8_t map_entry;
+	uint8_t map_sector[2];
 	uint8_t filetype;
 	uint8_t b20_2d[13];
 	uint8_t next_sector[2];
@@ -19,6 +66,10 @@ typedef struct cent_dirent_t {
 } cent_dirent_t;
 
 uint32_t sector_size = 400;
+
+static uint16_t read_be16(const uint8_t *value) {
+    return (value[0] << 8) | value[1];
+}
 
 void strip0x80(char * str, uint8_t n) {
 	for(int i=0; i<n; i++) {
@@ -51,6 +102,8 @@ int main(int argc, char **argv) {
 	    }
 	}
 
+	printf("Sector   Volume     File       Disk       Type MapStart MapSector MapEntry\n");
+
 	n=0;
 	do {
 		tsec=nsec;
@@ -68,47 +121,14 @@ int main(int argc, char **argv) {
 			return(-1);
 		}
 		n++;
-		//printf("ns0: 0x%2x  ns1: 0x%2x\n", de->next_sector[0], de->next_sector[1]);
-		nsec=(de->next_sector[0]<<8) | (de->next_sector[1]);
+		nsec = read_be16(de->next_sector);
 		strip0x80(de->volname,10);
 		strip0x80(de->filename,10);
 		strip0x80(de->diskname,10);
 		strip0x80(de->seqnum,2);
 
-		printf("Sector: 0x%06x\n", tsec);
-		printf("Volume Name: '%.10s'\n", de->volname);
-
-		printf(" 6 bytes (0x0a-0x0f):");
-		for(int i=0; i<6; i++) { printf("  0x%02x",de->b0a_0f[i]); }
-		printf("\n");
-
-		printf("File Mark: 0x%02x%02x\n", de->filemark[0], de->filemark[1]);
-
-		printf("File Name: '%.10s'\n", de->filename);
-
-		printf(" 3 bytes (0x1c-0x1e):");
-		for(int i=0; i<3; i++) { printf("  0x%02x",de->b1c_1e[i]); }
-		printf("\n");
-
-		printf("File type: 0x%02x\n", de->filetype);
-
-		printf("13 bytes (0x20-0x2d):");
-		for(int i=0; i<13; i++) { printf("  0x%02x",de->b20_2d[i]); }
-		printf("\n");
-
-		printf("Next File Start Sector: 0x%0x\n",nsec);
-
-		printf(" 1 byte       (0x2f):  0x%02x\n",de->b2f);
-
-		printf("Disk Name: '%.10s'\n", de->diskname);
-
-		printf("Seq Num: '%.2s'\n", de->seqnum);
-
-		printf(" 4 bytes (0x3d-0x3f):");
-		for(int i=0; i<4; i++) { printf("  0x%02x",de->b3d_3f[i]); }
-		printf("\n");
-
-		printf("\n");
+		printf("0x%06x %.10s %.10s %.10s 0x%02x 0x%06x 0x%06x  %d\n", tsec, de->volname, de->filename, de->diskname,
+		       de->filetype, read_be16(de->map_start_sector), read_be16(de->map_sector), de->map_entry);
 
 		if(!nsec) {
 			printf("\nFound %u entries.\n",n);
